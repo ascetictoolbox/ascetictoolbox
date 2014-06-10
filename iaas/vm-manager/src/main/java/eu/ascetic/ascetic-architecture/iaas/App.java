a486e982450cfafadff33095d3500bbd25efab0a
David Ortiz <david.ortiz@bsc.es>
2014-06-10 09:57:14 +0000

    Summary: Just testing the SVN repository.

---
+++ iaas/vm-manager/src/main/java/eu/ascetic/ascetic-architecture/iaas/App.java
@@ -0,0 +1,13 @@
+package eu.ascetic.ascetic-architecture.iaas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is just a test to make sure I can use TUB SVN." );
+    }
+}
