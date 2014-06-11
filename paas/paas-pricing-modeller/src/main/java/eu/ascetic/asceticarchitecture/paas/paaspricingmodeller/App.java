289c3ee61a8246740f60789ce027b330da3cc0a4
Eleni Agiatzidou <agiatzidou@aueb.gr>
2014-06-11 19:45:35 +0000

    Summary: <REQUIRED Add a brief Summary here> Comments: <OPTIONAL Add additional comments as needed>

---
+++ paas/paas-pricing-modeller/src/main/java/eu/ascetic/asceticarchitecture/paas/paaspricingmodeller/App.java
@@ -0,0 +1,13 @@
+package eu.ascetic.asceticarchitecture.paas.paaspricingmodeller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
