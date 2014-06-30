78760c7caabcbdfa4b344e6348e1d33a6fa79dec
Mario Macias <mario.macias@bsc.es>
2014-06-30 12:10:58 +0000

    restructuring things

--- paas/application-monitor/cube/bin/collector.js
+++
@@ -1,9 +0,0 @@
-var options = require("./collector-config"),
-    cube = require("../"),
-    server = cube.server(options);
-
-server.register = function(db, endpoints) {
-  cube.collector.register(db, endpoints);
-};
-
-server.start();
