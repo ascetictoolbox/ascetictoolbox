f71e6d5dc0deed0fdb19e2b53b1bd4fdb271b36d
Richard Kavanagh <scsrek@leeds.ac.uk>
2014-07-07 16:13:24 +0000

    Summary: Adding the ability to log data out from a WattsUp? meter to disk.

---
+++ utils/wattsup/src/main/java/eu/ascetic/utils/GenericLogger.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright 2014 University of Leeds
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package eu.ascetic.utils;
+
+import java.io.File;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This classes main purpose is to write the details of some entity to disk. It
+ * is however important that this doesn't slow anything else down or otherwise
+ * interrupt it.
+ *
+ * @author Richard Kavanagh
+ */
+public abstract class GenericLogger<Type> implements Runnable {
+
+    private LinkedBlockingDeque<Type> queue = new LinkedBlockingDeque<>();
+    private boolean stop = false;
+    ResultsStore saveFile = null;
+
+    private GenericLogger() {
+    }
+
+    /**
+     * This creates a generic reporting mechanism that records items out to
+     * disk, asynchronously with the main thread that has requested the report.
+     *
+     * @param file The file to write the item/s to
+     * @param overwrite If the file should be overwritten on the first write.
+     */
+    public GenericLogger(File file, boolean overwrite) {
+        saveFile = new ResultsStore(file);
+        if (overwrite && file.exists()) {
+            file.delete();
+        }
+    }
+
+    /**
+     * This prints a items value to file in an asynchronously fashion. i.e. the
+     * item's data is sent to a queue for printing to disk and then does
+     * not further interrupt the operations of the data logger tool.
+     *
+     * @param item The item to print to file.
+     */
+    public void printToFile(Type item) {
+        queue.add(item);
+    }
+
+    /**
+     * This starts the reporting process going. It is the main process that
+     * waits for item to arrive in a queue ready for printing.
+     */
+    @Override
+    public void run() {
+        Logger.getLogger(GenericLogger.class.getName()).log(Level.FINER, "The logger for the file {0} started.", saveFile.getResultsFile().getName());
+        while (!stop || !queue.isEmpty()) {
+            try {
+                Type currentItem = queue.poll(30, TimeUnit.SECONDS);
+                if (currentItem != null) {
+                    Logger.getLogger(GenericLogger.class.getName()).log(Level.FINER, "The logger for the file {0} wrote to disk.", saveFile.getResultsFile().getName());
+                    saveToDisk(saveFile, currentItem);
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(GenericLogger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        Logger.getLogger(GenericLogger.class.getName()).log(Level.INFO, "The schedule reporter for the file {0} has now stopped.", saveFile.getResultsFile().getName());
+    }
+
+    /**
+     * This writes an item out to disk for the purpose of auditing what is going
+     * on. It by default overwrites the previous file
+     *
+     * @param file The file to save the item to
+     * @param item The item to write to file
+     */
+    public void saveToDisk(File file, Type item) {
+        ResultsStore toDisk = new ResultsStore(file);
+        saveToDisk(toDisk, item);
+    }
+
+    /**
+     * This writes an item out to disk for the purpose of auditing what is
+     * going on.
+     *
+     * @param store The results store to save data to
+     * @param item The item to write to file
+     */
+    public void saveToDisk(ResultsStore store, Type item) {
+        try {
+            if (!store.getResultsFile().exists()) {
+                /**
+                 * Write out the header if the file does not exist or is to be
+                 * overwritten
+                 */
+                writeHeader(store);
+            }
+            writebody(item, store);
+            store.saveMemoryConservative();
+        } catch (Exception ex) {
+            //logging is important but should not stop the main thread from running!
+            Logger.getLogger(GenericLogger.class.getName()).log(Level.SEVERE, "An error occurred when saving an item to disk", ex);
+        }
+    }
+
+    /**
+     * This is to be overwritten. Use store.add for the first data item in the
+     * header and the store.append for each subsequent item. i.e.
+     * store.add("Time"); store.append("Temperature");
+     *
+     * @param store The ResultsStore to write out to disk with.
+     */
+    public abstract void writeHeader(ResultsStore store);
+
+    /**
+     * This is to be overwritten. Use store.add for the first data item in the
+     * header and the store.append for each subsequent item. i.e.
+     * store.add("Time"); store.append("Temperature");
+     *
+     * @param store The ResultsStore to write out to disk with.
+     */
+    /**
+     * This is to be overwritten. Use store.add for the first data item in the
+     * body and the store.append for each subsequent item. i.e.
+     * store.add(item.getValue()); store.append(item.getValue2());
+     *
+     * @param item
+     * @param store
+     */
+    public abstract void writebody(Type item, ResultsStore store);
+
+    /**
+     * This permanently stops the reporter. It will however report all queued
+     * work, before quitting.
+     */
+    public void stop() {
+        this.stop = true;
+    }
+}
---
+++ utils/wattsup/src/main/java/eu/ascetic/utils/Logger.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright 2014 University of Leeds
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package eu.ascetic.utils;
+
+import java.io.File;
+import java.io.IOException;
+import wattsup.jsdk.core.data.WattsUpConfig;
+import wattsup.jsdk.core.data.WattsUpPacket;
+import wattsup.jsdk.core.event.WattsUpDataAvailableEvent;
+import wattsup.jsdk.core.event.WattsUpDisconnectEvent;
+import wattsup.jsdk.core.event.WattsUpMemoryCleanEvent;
+import wattsup.jsdk.core.event.WattsUpStopLoggingEvent;
+import wattsup.jsdk.core.listener.WattsUpDataAvailableListener;
+import wattsup.jsdk.core.listener.WattsUpDisconnectListener;
+import wattsup.jsdk.core.listener.WattsUpMemoryCleanListener;
+import wattsup.jsdk.core.listener.WattsUpStopLoggingListener;
+import wattsup.jsdk.core.meter.WattsUp;
+
+/**
+ * This logs WattsUp meter data to disk in a continuous fashion.
+ *
+ */
+public class Logger {
+
+    private static WattsUp meter;
+
+    public static void main(String[] args) throws IOException {
+        final WattsUpLogger logger = new WattsUpLogger(new File("Dataset.csv"), false);
+        new Thread(logger).start();
+        String port = "COM9";
+        if (args.length > 0) {
+            port = args[0];
+        }
+        //Note: negative numbers for the schedule duration makes it run forever
+        meter = new WattsUp(new WattsUpConfig().withPort(port).scheduleDuration(-1).withInternalLoggingInterval(1).withExternalLoggingInterval(1));
+        System.out.println("WattsUp Meter Created");
+
+        meter.registerListener(new WattsUpDataAvailableListener() {
+            @Override
+            public void processDataAvailable(final WattsUpDataAvailableEvent event) {
+                WattsUpPacket[] values = event.getValue();
+                logger.printToFile(values);
+            }
+        });
+
+        meter.registerListener(new WattsUpMemoryCleanListener() {
+            @Override
+            public void processWattsUpReset(WattsUpMemoryCleanEvent event) {
+                System.out.println("Memory Just Cleaned");
+            }
+        });
+
+        meter.registerListener(new WattsUpStopLoggingListener() {
+            @Override
+            public void processStopLogging(WattsUpStopLoggingEvent event) {
+                System.out.println("Logging Stopped");
+                logger.stop();
+            }
+        });
+
+        meter.registerListener(new WattsUpDisconnectListener() {
+            @Override
+            public void onDisconnect(WattsUpDisconnectEvent event) {
+                System.out.println("Application Exiting Due to Disconnect");
+                logger.stop();
+            }
+        });
+
+        System.out.println("WattsUp Meter Connecting");
+        meter.connect(true);
+        meter.setLoggingModeSerial(1);
+        System.out.println("WattsUp Meter Connected " + meter.isConnected());
+
+    }
+}
