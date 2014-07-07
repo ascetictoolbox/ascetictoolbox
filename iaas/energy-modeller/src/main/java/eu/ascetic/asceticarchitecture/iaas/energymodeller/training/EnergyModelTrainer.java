12c84072b5b3ccaf7d703d34fcfb9b7324d17f5d
Eleni Agiatzidou <agiatzidou@aueb.gr>
2014-07-07 11:51:26 +0000

    Summary: <REQUIRED Add a brief Summary here> Comments: <OPTIONAL Add additional comments as needed>

--- iaas/energy-modeller/src/main/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/training/EnergyModelTrainer.java
+++
@@ -1,7 +0,0 @@
-package eu.ascetic.asceticarchitecture.iaas.energymodeller.training;
-
-public interface EnergyModelTrainer {
-	
-	public void TrainModelForHost (Host host, Collection<VM> virtualMachines);
-	
-}
