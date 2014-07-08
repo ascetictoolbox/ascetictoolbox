4ec648f94015dc924b5a47cb6f25188142da0e14
Jorge Ejarque <jorge.ejarque@bsc.es>
2014-07-08 10:27:46 +0000

    app uploader modified

--- saas/application-uploader/src/main/java/eu/ascetic/saas/application_uploader/ApplicationUploader.java
+++ saas/application-uploader/src/main/java/eu/ascetic/saas/application_uploader/ApplicationUploader.java
@@ -1,5 +1,8 @@
 package eu.ascetic.saas.application_uploader;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 
@@ -12,12 +15,18 @@ import eu.ascetic.paas.applicationmanager.model.Deployment;
 
 public class ApplicationUploader {
 	public static final String APPLICATIONS_PATH = "applications";
-
 	public static final String DEPLOYMENTS_PATH = "deployments";
+	public static final String AGREEMENT_PATH = "agreement";
+	public static final String ACCEPT_QP = "accept";
+	public static final String YES_VALUE = "yes";
+	public static final String NO_VALUE = "no";
 	
 	Client client;
 	WebResource resource;
 	
+	/** Constructor
+	 * @param rootURI URI to application manager root resource
+	 */
 	public ApplicationUploader(String rootURI){
 		super();
 		client = Client.create();
@@ -51,6 +60,76 @@ public class ApplicationUploader {
 			return response.getEntity(Deployment.class).getStatus();
 		}else
 			throw new ApplicationUploaderException("Error getting deployment status. Returned code is "+ response.getStatus());
+	}
+	
+	/** Get deployment agreement
+	 * @param applicationID Application Identifier
+	 * @param deploymentID Deployment Identifier
+	 * @return deployment status
+	 * @throws ApplicationUploaderException
+	 */
+	public String getDeploymentAgreement(String applicationID, String deploymentID) throws ApplicationUploaderException{
+		ClientResponse response = resource.path(APPLICATIONS_PATH).path(applicationID)
+				.path(DEPLOYMENTS_PATH).path(deploymentID).path(AGREEMENT_PATH).accept(MediaType.APPLICATION_XML_TYPE).get(ClientResponse.class);
+		if (response.getStatus() == ClientResponse.Status.OK.getStatusCode()) {
+			return response.getEntity(Deployment.class).getStatus();
+		}else
+			throw new ApplicationUploaderException("Error getting deployment agreement. Returned code is "+ response.getStatus());
+	}
+	
+	/** Get deployed vms
+	 * @param applicationID Application Identifier
+	 * @param deploymentID Deployment Identifier
+	 * @return deployed vms in different providers 
+	 *   [providerX->[ip_vm_1->ovf_vm_type_a,..., ip_vm_n->ovf_vm_type_b],
+	 *    ...
+	 *    providerY->[ip_vm_m->ovf_vm_type_c,..., ip_vm_z->ovf_vm_type_d]] 
+	 * @throws ApplicationUploaderException
+	 */
+	public Map<String,Map<String,String>> getDeployedVMs(String applicationID, String deploymentID) throws ApplicationUploaderException{
+		ClientResponse response = resource.path(APPLICATIONS_PATH).path(applicationID)
+				.path(DEPLOYMENTS_PATH).path(deploymentID).path(AGREEMENT_PATH).accept(MediaType.APPLICATION_XML_TYPE).get(ClientResponse.class);
+		if (response.getStatus() == ClientResponse.Status.OK.getStatusCode()) {
+			return getVMsFromDeployment(response.getEntity(Deployment.class));
+		}else
+			throw new ApplicationUploaderException("Error getting deployment agreement. Returned code is "+ response.getStatus());
+	}
+	
+	private Map<String, Map<String, String>> getVMsFromDeployment(
+			Deployment deployment) {
+		HashMap<String, Map<String, String>> vms = new  HashMap<String, Map<String, String>>();
+		// TODO: get vms from deployment providers currently
+		
+		return vms;
+	}
+
+	/** Accept deployment agreement
+	 * @param applicationID Application Identifier
+	 * @param deploymentID Deployment Identifier
+	 * @return deployment status
+	 * @throws ApplicationUploaderException
+	 */
+	public void acceptAgreement(String applicationID, String deploymentID) throws ApplicationUploaderException{
+		ClientResponse response = resource.path(APPLICATIONS_PATH).path(applicationID)
+				.path(DEPLOYMENTS_PATH).path(deploymentID).path(AGREEMENT_PATH).queryParam(ACCEPT_QP, YES_VALUE).put(ClientResponse.class);
+		if (response.getStatus() != ClientResponse.Status.ACCEPTED.getStatusCode()) {
+			throw new ApplicationUploaderException("Error accepting deployment agreement. Returned code is "+ response.getStatus());
+		}
+	}
+	
+	/** Reject deployment agreement
+	 * @param applicationID Application Identifier
+	 * @param deploymentID Deployment Identifier
+	 * @return deployment status
+	 * @throws ApplicationUploaderException
+	 */
+	public void rejectAgreement(String applicationID, String deploymentID) throws ApplicationUploaderException{
+		ClientResponse response = resource.path(APPLICATIONS_PATH).path(applicationID)
+				.path(DEPLOYMENTS_PATH).path(deploymentID).path(AGREEMENT_PATH)
+				.queryParam(ACCEPT_QP, NO_VALUE).put(ClientResponse.class);
+		if (response.getStatus() != ClientResponse.Status.ACCEPTED.getStatusCode()) {
+			throw new ApplicationUploaderException("Error accepting deployment agreement. Returned code is "+ response.getStatus());
+		}
 	}
 	
 	/** Undeploy an application deployment
@@ -92,6 +171,10 @@ public class ApplicationUploader {
 			throw new ApplicationUploaderException("Error deleting deployment. Returned code is "+ response.getStatus());
 		}
 	}
+	
+	
+	
+	
 	
 	
 }
