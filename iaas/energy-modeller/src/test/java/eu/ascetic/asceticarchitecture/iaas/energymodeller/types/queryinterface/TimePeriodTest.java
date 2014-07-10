849925e51ed87a3747d2cf1577c2dab00a11f0f6
Richard Kavanagh <scsrek@leeds.ac.uk>
2014-07-10 12:20:57 +0000

    Summary: Refactoring, moving the package of the class TimePeriod and removing the old package that it belonged to.

--- iaas/energy-modeller/src/test/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/types/queryinterface/TimePeriodTest.java
+++ iaas/energy-modeller/src/test/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/types/queryinterface/TimePeriodTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package eu.ascetic.asceticarchitecture.iaas.energymodeller.queryinterface;
+package eu.ascetic.asceticarchitecture.iaas.energymodeller.types.queryinterface;
 
 import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.TimePeriod;
 import java.util.Calendar;
