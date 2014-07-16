7d0be8941f80c6b2d87dc1193c525aa4f8ab0bb4
Richard Kavanagh <scsrek@leeds.ac.uk>
2014-07-16 11:58:49 +0000

    Summary: Fixing an error in the test file.

--- iaas/energy-modeller/src/test/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/datastore/DataGathererTest.java
+++ iaas/energy-modeller/src/test/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/datastore/DataGathererTest.java
@@ -1,11 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2014 University of Leeds
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 package eu.ascetic.asceticarchitecture.iaas.energymodeller.datastore;
 
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.calibration.Calibrator;
 import eu.ascetic.asceticarchitecture.iaas.energymodeller.queryinterface.datasourceclient.ZabbixDataSourceAdaptor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -40,7 +50,8 @@ public class DataGathererTest {
         System.out.println("run");
         ZabbixDataSourceAdaptor adaptor = new ZabbixDataSourceAdaptor();
         DefaultDatabaseConnector connector = new DefaultDatabaseConnector();
-        DataGatherer instance = new DataGatherer(adaptor, connector);
+        Calibrator calibrator = new Calibrator(adaptor);
+        DataGatherer instance = new DataGatherer(adaptor, connector, calibrator);
         Thread thread = new Thread(instance);
         thread.start();
         try {
