d6f25221018adfea8bacdbd9d708ae16ae171725
Richard Kavanagh <scsrek@leeds.ac.uk>
2014-07-17 15:08:07 +0000

    Summary: Improving the documentation of the energy modeller.

--- iaas/energy-modeller/src/main/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/energypredictor/DefaultEnergyPredictor.java
+++ iaas/energy-modeller/src/main/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/energypredictor/DefaultEnergyPredictor.java
@@ -32,17 +32,34 @@ public class DefaultEnergyPredictor extends AbstractEnergyPredictor {
 
     private EnergyModelTrainerInterface trainer = new DefaultEnergyModelTrainer();
 
+    /**
+     * This provides a prediction of how much energy is to be used by a host
+     *
+     * @param host The host to get the energy prediction for
+     * @param virtualMachines The virtual machines giving a workload on the host
+     * machine
+     * @return The prediction of the energy to be used.
+     */
     @Override
     public EnergyUsagePrediction getHostPredictedEnergy(Host host, Collection<VM> virtualMachines) {
         //TODO Write get host predicted energy i.e. implement the model here.
-       EnergyModel model = trainer.retrieveModel(host);
+        EnergyModel model = trainer.retrieveModel(host);
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
+    /**
+     * This provides a prediction of how much energy is to be used by a VM
+     *
+     * @param vm The vm to be deployed
+     * @param virtualMachines The virtual machines giving a workload on the host
+     * machine
+     * @param host The host that the VMs will be running on
+     * @return The prediction of the energy to be used.
+     */
     @Override
     public EnergyUsagePrediction getVMPredictedEnergy(VM vm, Collection<VM> virtualMachines, Host host) {
         //TODO Write get VM predicted energy i.e. implement the model here.
-       EnergyModel model = trainer.retrieveModel(host);
+        EnergyModel model = trainer.retrieveModel(host);
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
