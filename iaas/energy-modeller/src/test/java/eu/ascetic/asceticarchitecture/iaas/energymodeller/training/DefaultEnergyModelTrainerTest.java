b7439b90c14243b531d25d7687711cfa9c59be02
Eleni Agiatzidou <agiatzidou@aueb.gr>
2014-07-17 01:55:00 +0000

    Summary: Added DefaultEnergyModelTrainerTest.java Comments: <OPTIONAL Add additional comments as needed>

---
+++ iaas/energy-modeller/src/test/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/training/DefaultEnergyModelTrainerTest.java
@@ -0,0 +1,69 @@
+package eu.ascetic.asceticarchitecture.iaas.energymodeller.training;
+
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.Host;
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.HostEnergyCalibrationData;
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energymodel.EnergyModel;
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.training.DefaultEnergyModelTrainer;
+
+import java.util.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class DefaultEnergyModelTrainerTest 
+{
+   
+
+	public DefaultEnergyModelTrainerTest() {
+		
+	}
+	
+	 @BeforeClass
+	    public static void setUpClass() {
+	    }
+
+	 @AfterClass
+	    public static void tearDownClass() {
+	    }
+
+    @Before
+	    public void setUp() {
+	    }
+
+    @After
+	    public void tearDown() {
+	    }
+    
+    @Test   
+    public void testTrainModel() {
+    	DefaultEnergyModelTrainer trainer = new DefaultEnergyModelTrainer();
+    	EnergyModel model1=new EnergyModel();
+    	EnergyModel model2=new EnergyModel();
+    	Host host= new Host(1, "testHost");
+    	Host host2= new Host(2, "testHost2");
+    	boolean trained=false;
+    	double usageCPU=0.0;
+    	double usageRAM=0.0;
+    	double totalEnergyUsed=0.0;
+    	Random randomGenerator = new Random();
+    	for (int i=1; i<=10; i++){
+    		usageRAM=randomGenerator.nextDouble();
+    		usageCPU=randomGenerator.nextDouble();
+    		totalEnergyUsed=randomGenerator.nextDouble();
+    		trained=trainer.trainModel (host, usageCPU, usageRAM, totalEnergyUsed, 10);
+    		trainer.trainModel (host2, usageCPU, usageRAM, totalEnergyUsed, 10);
+    		
+    	}
+    	if (trained){
+    	model2=trainer.retrieveModel(host2);
+		System.out.println("model: "+ model2.getCoefCPU());
+    	}
+    }
+}
