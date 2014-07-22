d5141728d79babd3ee7ca7e1a3dbf87d7898969b
David Garcia Perez <david.garciaperez@atos.net>
2014-07-22 09:47:25 +0000

    adding manual integration test between MySQL and Application Manager

---
+++ paas/application-manager/src/test/java/eu/ascetic/paas/applicationmanager/dao/jpa/MysqJPATestIT.java
@@ -0,0 +1,121 @@
+package eu.ascetic.paas.applicationmanager.dao.jpa;
+
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import eu.ascetic.paas.applicationmanager.dao.ApplicationDAO;
+import eu.ascetic.paas.applicationmanager.dao.DeploymentDAO;
+import eu.ascetic.paas.applicationmanager.dao.VMDAO;
+import eu.ascetic.paas.applicationmanager.model.Application;
+import eu.ascetic.paas.applicationmanager.model.Deployment;
+import eu.ascetic.paas.applicationmanager.model.VM;
+
+
+
+public class MysqJPATestIT {
+	
+	public static void main(String args[]) throws InterruptedException {
+		// Load Spring configuration
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("/mysql-jpa-test-configuration.xml");
+		ApplicationDAO applicationDAO = (ApplicationDAO) context.getBean("ApplicationService");
+		DeploymentDAO deploymentDAO = (DeploymentDAO) context.getBean("DeploymentService");
+		VMDAO vmDAO = (VMDAO) context.getBean("VMService");
+		
+		int size = applicationDAO.getAll().size();
+		
+		System.out.println("Number of Applications in DB at Start: " + size);
+		
+		// We create an application
+		Application application = new Application();
+		application.setName("name");
+		
+		// We store the application
+		boolean saved = applicationDAO.save(application);
+		Application applicationFromDatabae = applicationDAO.getById(size+1);
+		size = applicationDAO.getAll().size();
+	
+		
+		System.out.println("Number of Applications in DB after storing an application: " + size);
+		
+		if(saved == false) {
+			System.out.println("IMPOSIBLE TO SAVE EXPERIMENT!!!!");
+			Thread.sleep(600000l);
+		}
+		
+		size = deploymentDAO.getAll().size();
+		
+		System.out.println("Number of Deployments in DB at Start: " + size);
+		
+		// We store a deployment
+		Deployment deployment = new Deployment();
+		deployment.setStatus("RUNNIG");
+		deployment.setPrice("expensive");
+		
+		saved = deploymentDAO.save(deployment);
+		
+		if(saved == false) {
+			System.out.println("IMPOSIBLE TO SAVE DEPLOYMENT!!!!");
+			Thread.sleep(600000l);
+		}
+		
+		Deployment deploymentFrDeployment = deploymentDAO.getById(size+1);
+		size = deploymentDAO.getAll().size();
+		
+		System.out.println("Number of Deployments in DB after storing one: " + size);
+		
+		size = vmDAO.getAll().size();
+		
+		System.out.println("Number of VMs in DB at Start: " + size);
+		
+		VM vm = new VM();
+		vm.setIp("127.0.0.1");
+		vm.setOvfId("ovf-id");
+		vm.setProviderId("provider-id");
+		vm.setProviderVmId("provider-vm-id");
+		vm.setSlaAgreement("sla-agreement");
+		vm.setStatus("RUNNING");
+		
+		saved = vmDAO.save(vm);
+		
+		if(saved == false) {
+			System.out.println("IMPOSIBLE TO SAVE VM!!!!");
+			Thread.sleep(600000l);
+		}
+		
+		VM vmFromDatabase = vmDAO.getById(size+1);
+		
+		size = vmDAO.getAll().size();
+		
+		System.out.println("Number of VMs in DB after storing one: " + size);
+		
+		applicationFromDatabae = applicationDAO.getById(applicationFromDatabae.getId());
+		deploymentFrDeployment = deploymentDAO.getById(deploymentFrDeployment.getId());
+		vmFromDatabase = vmDAO.getById(vmFromDatabase.getId());
+		
+		applicationFromDatabae.addDeployment(deploymentFrDeployment);
+		deploymentFrDeployment.addVM(vmFromDatabase);
+		
+		boolean updated = applicationDAO.update(applicationFromDatabae);
+		
+		if(updated == false) {
+			System.out.println("IMPOSIBLE TO UPDATE APPLICATION!!!!");
+			Thread.sleep(600000l);
+		}
+		
+		boolean deleted = applicationDAO.delete(applicationFromDatabae);
+		
+		if(deleted == false) {
+			System.out.println("IMPOSIBLE TO DELETE APPLICATION!!!!");
+			Thread.sleep(600000l);
+		}
+	}
+}
+
