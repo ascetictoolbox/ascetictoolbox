2696b4ee058d862608d7a869e040a4cc7fc47cfc
Michael Kammer <michael.kammer@tu-berlin.de>
2014-07-22 16:07:32 +0000

    AManager with Tomcat

--- utils/chef/cookbooks/ascetic-paas/files/default/am-installer.sh
+++ utils/chef/cookbooks/ascetic-paas/files/default/am-installer.sh
@@ -3,13 +3,14 @@
 user=$(id -un)
 home=$(getent passwd | grep $user | awk -F: '{print $6}')
 
-svn_root="https://ascetic-dev.cit.tu-berlin.de/svn/trunk/utils/application-manager-database"
+svn_amdb_root="https://ascetic-dev.cit.tu-berlin.de/svn/trunk/utils/application-manager-database"
+svn_conf_root="https://ascetic-dev.cit.tu-berlin.de/svn/trunk/paas/application-manager/src/main/resources/"
 mysql_conf="/etc/mysql/debian.cnf"
 
 is_updated_db_scheme() {
   [ -z "$1" ] && exit 1
   tmp=$(mktemp)
-  if ! wget "$svn_root/db/changelog/db.changelog-master.xml" -O "$tmp" --no-check-certificate 1>/dev/null 2>&1; then
+  if ! wget "$svn_amdb_root/db/changelog/db.changelog-master.xml" -O "$tmp" --no-check-certificate 1>/dev/null 2>&1; then
     echo "Unable to get DB changelog"
     exit 1
   fi
@@ -26,26 +27,50 @@ is_updated_db_scheme() {
 mkdir -p $home/amanager
 if ! is_updated_db_scheme $home/amanager/db.changelog-master.xml; then
   echo "DB unchanged"
-  exit 0
-fi
-
-echo "Creating MySQL user and DB"
-sudo mysql --defaults-extra-file="$mysql_conf" << END
+else
+  echo "Creating MySQL user and DB"
+  sudo mysql --defaults-extra-file="$mysql_conf" << END
 CREATE DATABASE IF NOT EXISTS application_manager;
 GRANT ALL ON application_manager.* TO 'app-manager'@'%' IDENTIFIED BY 'ascetic-secret';
 END
 
-echo "Using liquibase to update DB"
-tmp=$(mktemp -d)
-svn export --force --non-interactive --trust-server-cert "$svn_root"/ "$tmp"
+  echo "Using liquibase to update DB"
+  tmp=$(mktemp -d)
+  svn export --force --non-interactive --trust-server-cert "$svn_amdb_root"/ "$tmp"
 
-opwd=$(pwd)
-cd "$tmp"
+  opwd=$(pwd)
+  cd "$tmp"
 
-LIQUIBASE_HOME="$tmp/liquibase" liquibase/liquibase --driver=com.mysql.jdbc.Driver --changeLogFile=db/changelog/db.changelog-master.xml --url="jdbc:mysql://localhost/application_manager" --username="app-manager" --password="ascetic-secret" migrate
+  LIQUIBASE_HOME="$tmp/liquibase" liquibase/liquibase --driver=com.mysql.jdbc.Driver --changeLogFile=db/changelog/db.changelog-master.xml --url="jdbc:mysql://localhost/application_manager" --username="app-manager" --password="ascetic-secret" migrate
 
-cd "$opwd"
-rm "$tmp" -Rf
+  cd "$opwd"
+  rm "$tmp" -Rf
+fi
+
+sudo mkdir -p /etc/ascetic/paas/application-manager/
+sudo chmod 755 /etc/ascetic /etc/ascetic/paas/
+sudo chown $user /etc/ascetic/paas/application-manager/
+
+curl -k https://ascetic-dev.cit.tu-berlin.de/svn/trunk/paas/application-manager/src/main/resources/application-manager.properties |
+ sed 's/^vm-manager.url=http:\/\/.*:/vm-manager.url=http:\/\/iaas-vm-dev:/g' |
+ tee /etc/ascetic/paas/application-manager/application-manager.properties >/dev/null
+
+curl -k https://ascetic-dev.cit.tu-berlin.de/svn/trunk/paas/application-manager/src/main/resources/applicationContext.xml |
+ sed 's/p:username="[^"]*"/p:username="app-manager"/g' | sed 's/p:password="[^"]*"/p:password="ascetic-secret"/g' |
+ sed 's/p:url="[^"]*"/p:url="jdbc:mysql:\/\/localhost:3000\/application_manager"/g' |
+ tee /etc/ascetic/paas/application-manager/applicationContext.xml >/dev/null
+
+curl -k https://ascetic-dev.cit.tu-berlin.de/svn/trunk/paas/application-manager/src/main/resources/META-INF/persistence.mysql.xml |
+ sed 's/name="hibernate.connection.url" value="[^"]"/name="hibernate.connection.url" value="jdbc:mysql:\/\/localhost:3000\/provider_registry"/g' |
+ sed 's/name="hibernate.connection.username" value="[^"]*"/name="hibernate.connection.username" value="provider-reg"/g' |
+ sed 's/name="hibernate.connection.password" value="[^"]*"/name="hibernate.connection.password" value="ascetic-secret"/g' |
+ tee /etc/ascetic/paas/application-manager/persistence.mysql.xml >/dev/null
+
+# TODO: Be more restrictive (chgrp tomcat, chmod o-rwx)
+chmod 755 /etc/ascetic/paas/application-manager/
+chmod 644 /etc/ascetic/paas/application-manager/*
+
+curl -k 'https://ascetic-jenkins.cit.tu-berlin.de/job/ASCETiC%20Reference%20Architecture/ws/trunk/paas/application-manager/target/application-manager-0.1-SNAPSHOT.war' | sudo tee /var/lib/tomcat7/webapps/application-manager.war > /dev/null || exit 1
 
 exit 0
 
