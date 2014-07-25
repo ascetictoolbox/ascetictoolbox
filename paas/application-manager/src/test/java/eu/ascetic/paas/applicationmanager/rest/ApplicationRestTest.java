8a369cac28e85e1cb8e186f3e3495969d1955fa7
David Garcia Perez <david.garciaperez@atos.net>
2014-07-25 13:29:25 +0000

    adding workflow controller

--- paas/application-manager/src/test/java/eu/ascetic/paas/applicationmanager/rest/ApplicationRestTest.java
+++ paas/application-manager/src/test/java/eu/ascetic/paas/applicationmanager/rest/ApplicationRestTest.java
@@ -1,11 +1,11 @@
 package eu.ascetic.paas.applicationmanager.rest;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.argThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 
 import java.io.File;
 import java.io.IOException;
@@ -26,8 +26,6 @@ import javax.xml.bind.Unmarshaller;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.ArgumentMatcher;
 
 import eu.ascetic.paas.applicationmanager.dao.ApplicationDAO;
 import eu.ascetic.paas.applicationmanager.model.Application;
@@ -161,13 +159,47 @@ public class ApplicationRestTest {
 		assertEquals(MediaType.APPLICATION_XML, applicationResponse.getLinks().get(2).getType());
 	}
 	
+	
+	@Test
+	public void postAnApplicationInDB() throws JAXBException {
+		ApplicationDAO applicationDAO = mock(ApplicationDAO.class);
+		
+		Application application = new Application();
+		application.setId(1);
+		application.setName("Three Tier Web App");
+		
+		// We put in order the different calls to the DB
+		when(applicationDAO.getByName("Three Tier Web App")).thenReturn(application, application);
+		when(applicationDAO.update(any(Application.class))).thenReturn(true);
+		
+		ApplicationRest applicationRest = new ApplicationRest();
+		applicationRest.applicationDAO = applicationDAO;
+		
+		Response response = applicationRest.postApplication(threeTierWebAppOvfString);
+		assertEquals(201, response.getStatus());
+		
+		String xml = (String) response.getEntity();
+		
+		JAXBContext jaxbContext = JAXBContext.newInstance(Application.class);
+		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+		Application applicationResponse = (Application) jaxbUnmarshaller.unmarshal(new StringReader(xml));
+		
+		// We verify the application was stored correctly
+		assertEquals(1, applicationResponse.getId());
+		assertEquals("/applications/1", applicationResponse.getHref());
+		assertEquals("Three Tier Web App", applicationResponse.getName());
+		assertEquals(1, applicationResponse.getDeployments().size());
+		assertEquals(threeTierWebAppOvfString, applicationResponse.getDeployments().get(0).getOvf());
+		assertEquals(Dictionary.APPLICATION_STATUS_SUBMITTED, applicationResponse.getDeployments().get(0).getStatus());
+		
+		// We verify the number of calls to the DAO
+		verify(applicationDAO, times(2)).getByName("Three Tier Web App");
+		verify(applicationDAO, times(1)).update(any(Application.class));
+	}
+	
 	@Test
 	public void postAnApplicationNotInDB() throws JAXBException {
 		ApplicationDAO applicationDAO = mock(ApplicationDAO.class);
-		
-		// We put in order the different calls to the DB
-		when(applicationDAO.getByName("Three Tier Web App")).thenReturn(null);
-		when(applicationDAO.save(any(Application.class))).thenReturn(true) ;
 		
 		Application application = new Application();
 		application.setId(1);
@@ -178,7 +210,9 @@ public class ApplicationRestTest {
 		deployment.setStatus(Dictionary.APPLICATION_STATUS_SUBMITTED);
 		application.addDeployment(deployment);
 		
-		when(applicationDAO.getByName("Three Tier Web App")).thenReturn(application);
+		// We put in order the different calls to the DB
+		when(applicationDAO.getByName("Three Tier Web App")).thenReturn(null, application);
+		when(applicationDAO.save(any(Application.class))).thenReturn(true);
 		
 		ApplicationRest applicationRest = new ApplicationRest();
 		applicationRest.applicationDAO = applicationDAO;
@@ -197,8 +231,9 @@ public class ApplicationRestTest {
 		assertEquals("/applications/1", applicationResponse.getHref());
 		assertEquals("Three Tier Web App", applicationResponse.getName());
 		
-		// We verify that the argument sent was the correct one
-		verify(applicationDAO).save((Application) argThat(new MessagesApplicationArgumentMatcher()));
+		// We verify the number of calls to the DAO
+		verify(applicationDAO, times(2)).getByName("Three Tier Web App");
+		verify(applicationDAO, times(1)).save(any(Application.class));
 	}
 	
 	
@@ -213,6 +248,18 @@ public class ApplicationRestTest {
 		assertEquals("Invalid OVF", message);
 	}
 	
+	@Test
+	public void addDeploymentToApplicationTest() {
+		ApplicationRest applicationRest = new ApplicationRest();
+			
+		String ovf = "ovf";
+		
+		Deployment deployment = applicationRest.createDeploymentToApplication(ovf);
+
+		assertEquals("ovf", deployment.getOvf());
+		assertEquals(Dictionary.APPLICATION_STATUS_SUBMITTED, deployment.getStatus());
+	}
+	
 	/**
 	 * It just reads a file form the disk... 
 	 * @param path
@@ -223,19 +270,5 @@ public class ApplicationRestTest {
 	protected String readFile(String path, Charset encoding) throws IOException {
 		byte[] encoded = Files.readAllBytes(Paths.get(path));
 		return new String(encoded, encoding);
-	}
-}
-
-class MessagesApplicationArgumentMatcher extends ArgumentMatcher<Application> {
-
-	public boolean matches(Object o) {
-		//	45             if (o instanceof List) {
-		//	46                 List<String> strings = (List<String>) o;
-		//	47                 if (strings.size() != 3) return false;
-		//	48                 if (!strings.get(0).equals("<message><name>Joe Smith</name></message>")) return false;
-		//	49                 if (!strings.get(1).equals("<message><name>John Doe</name></message>")) return false;
-		//	                 if (!strings.get(2).equals("<message><name>Eddie Moola</name></message>")) return false;
-		//	            }
-		return true;
 	}
 }
