1ed29a4bed16f9fcabc65cddfb3115218c29a3a6
David Rojo <david.rojoa@atos.net>
2014-08-14 11:56:11 +0000

    VM contextualizer client

---
+++ paas/application-manager/src/main/java/eu/ascetic/paas/applicationmanager/contextualizer/VmcClient.java
@@ -0,0 +1,33 @@
+package eu.ascetic.paas.applicationmanager.contextualizer;
+
+import org.apache.log4j.Logger;
+
+import eu.ascetic.paas.applicationmanager.conf.Configuration;
+import eu.ascetic.utils.ovf.api.OvfDefinition;
+import eu.ascetic.vmc.api.VmcApi;
+import eu.ascetic.vmc.api.datamodel.GlobalConfiguration;
+
+public class VmcClient {
+	
+	private VmcApi vmcApi;
+	private static Logger logger = Logger.getLogger(VmcClient.class);
+	
+	public VmcClient(OvfDefinition ovfDefinition){
+		GlobalConfiguration globalConfiguration = null;
+		try {
+			globalConfiguration = new GlobalConfiguration(Configuration.vmcontextualizerConfigurationFileDirectory);
+		} catch (Exception e) {
+			logger.info("Error creating globalConfiguration object to connect with VM contextualizer. Details: " + e.getMessage());
+		}
+		if (globalConfiguration != null){
+			vmcApi = new VmcApi(globalConfiguration);
+		}
+	}
+	
+	public VmcApi getVmcClient(){
+		return vmcApi;
+	}
+
+	
+	
+}
