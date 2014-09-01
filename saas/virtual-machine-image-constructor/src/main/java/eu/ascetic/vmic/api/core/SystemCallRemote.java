f830c9255a12ab90691ddec3a51d70e42f55feb8
Django Armstrong <scsdja@leeds.ac.uk>
2014-09-01 15:36:15 +0000

    Summary: Still fixing VMIC escaping bugs.

--- saas/virtual-machine-image-constructor/src/main/java/eu/ascetic/vmic/api/core/SystemCallRemote.java
+++ saas/virtual-machine-image-constructor/src/main/java/eu/ascetic/vmic/api/core/SystemCallRemote.java
@@ -124,14 +124,12 @@ public class SystemCallRemote extends SystemCall {
         }
 
         sshCommand = sshCommand.replace("'", "\\'");
-        //sshCommand = sshCommand.replace("\"", "\\\"");
         sshCommand = sshCommand.replace("[", "\\[");
         sshCommand = sshCommand.replace("]", "\\]");
 
-        // If using cygwin then we can skip the following escaping as the
-        // windows shell ignores these chars anyway
-        if (!sshPath.contains("cygwin")) {
-            //sshCommand = sshCommand.replace("$", "\\$");
+        // If using cygwin then do the following escaping
+        if (sshPath.contains("cygwin")) {
+            sshCommand = sshCommand.replace("\"", "\\\"");
         }
 
         // Construct the SSH command
