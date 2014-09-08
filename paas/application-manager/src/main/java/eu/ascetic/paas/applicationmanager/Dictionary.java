f750655dffdef54a1cab6a98ae7b4482a9a1c880
David Rojo <david.rojoa@atos.net>
2014-09-08 15:31:53 +0000

    Included constants to calculate the disk size specified in OVF in GB, taking in account the capacity and the units

--- paas/application-manager/src/main/java/eu/ascetic/paas/applicationmanager/Dictionary.java
+++ paas/application-manager/src/main/java/eu/ascetic/paas/applicationmanager/Dictionary.java
@@ -3,4 +3,8 @@ package eu.ascetic.paas.applicationmanager;
 public class Dictionary {
 	public static final String CONTENT_TYPE_JSON = "application/json";
 	public static final String USER_AGENT = "ASCETiC-Application-Manager/0.1";
+	
+	public static String DISK_SIZE_UNIT_GBYTE =  "byte * 2^30";
+	public static String DISK_SIZE_UNIT_MBYTE =  "byte * 2^20";
+	public static String DISK_SIZE_UNIT_KBYTE =  "byte * 2^10";
 }
