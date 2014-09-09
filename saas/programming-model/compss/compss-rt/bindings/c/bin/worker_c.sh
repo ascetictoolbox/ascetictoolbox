ecf48472935c2b16124ec3b94c374bf3a5d79458
Francesc Lordan <francesc.lordan@bsc.es>
2014-09-09 16:13:11 +0000

    Programming model:remove bindings

--- saas/programming-model/compss/compss-rt/bindings/c/bin/worker_c.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-scriptDir=`dirname $0`
-export LD_LIBRARY_PATH=$scriptDir/../../bindings/c/lib:$scriptDir/../../bindings/bindings-common/lib:$LD_LIBRARY_PATH
-app_dir=$1
-exec $app_dir/worker_c $@
