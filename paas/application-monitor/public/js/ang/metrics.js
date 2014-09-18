225e08e694830608f242475a6b2878de5deccf1d
Mario Macias <mario.macias@bsc.es>
2014-09-18 15:40:58 +0000

    GUI: Modal form for new metrics graph

--- paas/application-monitor/public/js/ang/metrics.js
+++ paas/application-monitor/public/js/ang/metrics.js
@@ -1,15 +1,13 @@
 (function() {
-	var appip = angular.module("Metrics",["ui.bootstrap"]);
+	var appip = angular.module("Metrics",["ui.bootstrap","highcharts-ng"]);
 
 	appip.time = 0;
 
 	appip.controller("GraphicsPanelController", ["$modal","$scope", function($modal, $scope) {
-        $scope.panels = {"0":{"title":"Test Panel"}};
-        $scope.idCounter = 1;
+        $scope.panels = { };
+        $scope.idCounter = 0;
 
         $scope.addNewPanel = function() {
-            console.log("Ke pasa tron");
-
             var modalInstance = $modal.open({
                 templateUrl: 'newPanelForm.html',
                 controller : "NewPanelFrame",
@@ -19,14 +17,13 @@
             });
 
             modalInstance.result.then(function(form) {
-                console.log("vete a la mierda");
-                console.log(form);
+                $scope.panels[$scope.idCounter] = form;
+                $scope.idCounter++;
             }, function() {
                 console.log("cabronazo");
             });
 
-            $scope.panels[this.idCounter] = {"title":"hola tron " + this.idCounter};
-            $scope.idCounter++;
+
 		};
 
 	}]);
@@ -47,6 +44,81 @@
         $scope.cancel = function() {
             $modalInstance.dismiss('cancel');
         }
+    }]);
+
+    appip.controller("TimeSeriesController", ["$scope", "$interval", function($scope, $interval) {
+
+        Highcharts.setOptions({
+            global: {
+                useUTC: false
+            }
+        });
+
+        $scope.highchartsNG = {
+            chart: {
+                animation : false
+            },
+            title: {
+                text: 'USD to EUR exchange rate from 2006 through 2008'
+            },
+            xAxis: {
+                type: 'datetime'
+            },
+            yAxis: {
+                title: {
+                    text: 'Exchange rate'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+
+            series: [{
+                data: (function () {
+                    // generate an array of random data
+                    var data = [],
+                        time = (new Date()).getTime(),
+                        i;
+
+                    for (i = -19; i <= 0; i += 1) {
+                        data.push({
+                            x: time + i * 1000,
+                            y: Math.random()
+                        });
+                    }
+                    return data;
+                }())
+            }]
+        };
+
+        $interval(function() {
+            var x = (new Date()).getTime(), // current time
+                y = Math.random();
+            console.log(x + " - " , y);
+            var series = $scope.highchartsNG.series[0];
+            series.data.shift();
+            series.data.push({"x":x,"y":y});
+            //series.addPoint([x, y], true, true);
+        },3000);
+
+
+
+
+
+        /*{
+            options: {
+                chart: {
+                    type: 'bar'
+                }
+            },
+            series: [{
+                data: [10, 15, 12, 8, 7]
+            }],
+            title: {
+                text: 'Hello'
+            },
+            loading: false
+        }*/
     }]);
 
 })();
