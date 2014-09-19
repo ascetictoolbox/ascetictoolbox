55235caa31f58603d723d6503c76db25227ee990
Christophe Ponsard <cp@cetic.be>
2014-09-19 13:45:40 +0000

    license information

--- saas/SaaSMonitorController/src/eu/ascetic/saas/monitorcontroller/UserInfo.java
+++ saas/SaaSMonitorController/src/eu/ascetic/saas/monitorcontroller/UserInfo.java
@@ -1,5 +1,22 @@
 package eu.ascetic.saas.monitorcontroller;
 
+/**
+ * Copyright (C) 2014 CETIC (Centre d'Excellence en Technologies de l'Information et de la Communication)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
