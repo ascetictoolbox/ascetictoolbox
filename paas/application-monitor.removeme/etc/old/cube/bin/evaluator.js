13464fdfcb31fd8dea173bb0ab34ad2fd75e3fe6
Michael Kammer <michael.kammer@tu-berlin.de>
2014-09-23 14:56:32 +0000

    Removed unused application-monitor.removeme directory

--- paas/application-monitor.removeme/etc/old/cube/bin/evaluator.js
+++
@@ -1,9 +0,0 @@
-var options = require("./evaluator-config"),
-    cube = require("../"),
-    server = cube.server(options);
-
-server.register = function(db, endpoints) {
-  cube.evaluator.register(db, endpoints);
-};
-
-server.start();
--- paas/application-monitor.removeme/public/js/ang/global-view.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	var globalView = angular.module("GlobalView");
-
-
-
-})();
