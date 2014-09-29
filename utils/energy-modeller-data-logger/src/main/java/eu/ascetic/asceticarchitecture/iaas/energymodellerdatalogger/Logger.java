cc27d0b5087851136e430f9e186a12f1ef5b27b3
Richard Kavanagh <scsrek@leeds.ac.uk>
2014-09-29 14:18:03 +0000

    Summary: Making the data logger act as a console app, writing to disk continuously until stopped.

--- utils/energy-modeller-data-logger/src/main/java/eu/ascetic/asceticarchitecture/iaas/energymodellerdatalogger/Logger.java
+++ utils/energy-modeller-data-logger/src/main/java/eu/ascetic/asceticarchitecture/iaas/energymodellerdatalogger/Logger.java
@@ -13,35 +13,64 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package eu.ascetic.asceticarchitecture.iaas.energymodellerdatalogger;
 
 import eu.ascetic.asceticarchitecture.iaas.energymodeller.queryinterface.datasourceclient.ZabbixDataSourceAdaptor;
 import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.Host;
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.logging.Level;
 
 /**
- * 
- *
+ * This application logs out the raw data that is received by the energy
+ * modeller from the Zabbix API.
  */
-public class Logger 
-{
-    public static void main( String[] args )
-    {
-         MeasurementLogger logger = new MeasurementLogger(new File("Dataset.txt"), false);
-         new Thread(logger).start();
-         ZabbixDataSourceAdaptor adaptor = new ZabbixDataSourceAdaptor();
-         Host host = adaptor.getHostByName("asok12");
-         for(int i = 0; i < 10; i++) {
+public class Logger {
+
+    private static boolean running = true;
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length == 0) {
+            System.out.println("Please provide as the first argument the name of "
+                    + "the host or virtual machine to monitor, such as asok09.");
+            System.exit(0);
+        }
+        String hostname = args[0];
+        System.out.println("This application will run continually until the word "
+                + "'quit' is written.)");
+        System.out.println("It is currently logging data out for: " + hostname);
+        System.out.println("This is being output to the file: Dataset_" + hostname + ".txt");
+        MeasurementLogger logger = new MeasurementLogger(new File("Dataset_" + hostname + ".txt"), false);
+        new Thread(logger).start();
+        QuitWatcher quitWatcher = new QuitWatcher();
+        new Thread(quitWatcher).start();
+        ZabbixDataSourceAdaptor adaptor = new ZabbixDataSourceAdaptor();
+        Host host = adaptor.getHostByName(hostname);
+        while (running) {
             logger.printToFile(adaptor.getHostData(host));
-             try {
-                 Thread.sleep(5000);
-             } catch (InterruptedException ex) {
-                 java.util.logging.Logger.getLogger(Logger.class.getName()).log(Level.SEVERE, null, ex);
-             }
-         }
-         logger.stop();
-        
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                java.util.logging.Logger.getLogger(Logger.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        logger.stop();
+    }
+
+    private static class QuitWatcher implements Runnable {
+
+        @Override
+        public void run() {
+            while (running) {
+                Scanner scanner = new Scanner(System.in);
+                String cmd = scanner.hasNext() ? scanner.next() : null;
+                if (cmd != null && cmd.equals("quit")) {
+                    running = false;
+                }
+            }
+        }
+
     }
 }
