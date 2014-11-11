12159724b4cb38d70d735ba6a8c78cf3c3307ee9
David Ortiz <david.ortiz@bsc.es>
2014-11-11 09:47:43 +0000

    VMM. Forgot to include 1 file in the previous commit.

---
+++ iaas/virtual-machine-manager/opta-vm-placement/src/main/java/es/bsc/vmplacement/lib/OptaVmPlacement.java
@@ -0,0 +1,56 @@
+/**
+ Copyright (C) 2013-2014  Barcelona Supercomputing Center
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package es.bsc.vmplacement.lib;
+
+import es.bsc.vmplacement.domain.*;
+import es.bsc.vmplacement.placement.config.VmPlacementConfig;
+
+import java.util.List;
+
+/**
+ * @author David Ortiz (david.ortiz@bsc.es)
+ */
+public interface OptaVmPlacement {
+
+    /**
+     * Given a list of hosts, a list of VMs, applies the best placement that can be found according to the
+     * configuration parameters specified.
+     *
+     * @param hosts the hosts
+     * @param vms the VMs
+     * @param config the configuration parameters for the placement
+     * @return the state of the cluster after applying the best placement that could be found
+     */
+    public ClusterState getBestSolution(List<Host> hosts, List<Vm> vms, VmPlacementConfig config);
+
+    /**
+     * Returns a list of the construction heuristics that are supported by the library.
+     *
+     * @return List of the construction heuristics
+     */
+    public List<ConstructionHeuristic> getConstructionHeuristics();
+
+    /**
+     * Returns a list of the local search algorithm that are supported by the library.
+     *
+     * @return the list of local search algorithms
+     */
+    public List<LocalSearchAlgorithm> getLocalSearchAlgorithms();
+
+}
