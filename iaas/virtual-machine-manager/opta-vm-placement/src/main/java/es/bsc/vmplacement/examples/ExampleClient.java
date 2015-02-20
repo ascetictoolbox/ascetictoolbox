f78fc4e96692a7dffb90753bff306db22db2db21
David Ortiz <david.ortiz@bsc.es>
2015-02-20 08:53:07 +0000

    VMM. Minor change.

--- iaas/virtual-machine-manager/opta-vm-placement/src/main/java/es/bsc/vmplacement/examples/ExampleClient.java
+++ iaas/virtual-machine-manager/opta-vm-placement/src/main/java/es/bsc/vmplacement/examples/ExampleClient.java
@@ -16,7 +16,7 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-package es.bsc.vmplacement.clients;
+package es.bsc.vmplacement.examples;
 
 import es.bsc.vmplacement.domain.ConstructionHeuristic;
 import es.bsc.vmplacement.domain.Host;
