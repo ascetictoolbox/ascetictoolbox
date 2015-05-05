9979a41d6d098ab85245b719f1a9d7672ac94993
David Ortiz <david.ortiz@bsc.es>
2015-05-05 12:29:15 +0000

    VMM client. Added image calls to the service.

--- iaas/virtual-machine-manager/vmm-client/src/main/java/es/bsc/vmmclient/rest/VmmService.java
+++ iaas/virtual-machine-manager/vmm-client/src/main/java/es/bsc/vmmclient/rest/VmmService.java
@@ -1,26 +1,24 @@
 /**
- Copyright (C) 2013-2014  Barcelona Supercomputing Center
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Lesser General Public
- License as published by the Free Software Foundation; either
- version 2.1 of the License, or (at your option) any later version.
-
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Lesser General Public License for more details.
-
- You should have received a copy of the GNU Lesser General Public
- License along with this library; if not, write to the Free Software
- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Copyright (C) 2013-2014  Barcelona Supercomputing Center
+ * <p/>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 package es.bsc.vmmclient.rest;
 
-import es.bsc.vmmclient.models.ListVmsDeployed;
-import es.bsc.vmmclient.models.Vm;
-import es.bsc.vmmclient.models.VmDeployed;
+import es.bsc.vmmclient.models.*;
 import retrofit.http.DELETE;
 import retrofit.http.GET;
 import retrofit.http.POST;
@@ -49,5 +47,17 @@ public interface VmmService {
 
     @DELETE("/vmsapp/{appId}")
     void destroyAppVms(@Path("appId") String id);
+
+    @GET("/images")
+    List<ImageUploaded> getImages();
+
+    @POST("/images")
+    String uploadImage(ImageToUpload image);
+
+    @GET("/images/{id}")
+    ImageUploaded getImage(@Path("id") String id);
+
+    @DELETE("/images/{id}")
+    void destroyImage(@Path("id") String id);
 
 }
