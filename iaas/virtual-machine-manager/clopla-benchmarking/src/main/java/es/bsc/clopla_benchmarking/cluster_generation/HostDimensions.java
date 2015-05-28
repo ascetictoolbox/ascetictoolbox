13ece09500367f140b334acd2846b9e5099e0fac
David Ortiz <david.ortiz@bsc.es>
2015-05-28 12:55:12 +0000

    Add some checkings in HostDimensions class

--- iaas/virtual-machine-manager/clopla-benchmarking/src/main/java/es/bsc/clopla_benchmarking/cluster_generation/HostDimensions.java
+++ iaas/virtual-machine-manager/clopla-benchmarking/src/main/java/es/bsc/clopla_benchmarking/cluster_generation/HostDimensions.java
@@ -2,6 +2,8 @@ package es.bsc.clopla_benchmarking.cluster_generation;
 
 import com.google.common.base.MoreObjects;
 
+import static com.google.common.base.Preconditions.checkArgument;
+
 public class HostDimensions {
 
     private final int minCpus;
@@ -13,6 +15,7 @@ public class HostDimensions {
 
     public HostDimensions(int minCpus, int maxCpus, int minRamGb, int maxRamGb,
                           int minDiskGb, int maxDiskGb) {
+        checkConstructorParams(minCpus, maxCpus, minRamGb, maxRamGb, minDiskGb, maxDiskGb);
         this.minCpus = minCpus;
         this.maxCpus = maxCpus;
         this.minRamGb = minRamGb;
@@ -55,6 +58,16 @@ public class HostDimensions {
                 .add("minDiskGb", minDiskGb)
                 .add("maxDiskGb", maxDiskGb)
                 .toString();
+    }
+
+    private void checkConstructorParams(int minCpus, int maxCpus, int minRamGb, int maxRamGb,
+                                        int minDiskGb, int maxDiskGb) {
+        checkArgument(minCpus >= 0, "minCpus needs to be >= 0");
+        checkArgument(maxCpus >= 0, "maxCpus needs to be >= 0");
+        checkArgument(minRamGb >= 0, "minRamGb needs to be >= 0");
+        checkArgument(maxRamGb >= 0, "maxRamGb needs to be >= 0");
+        checkArgument(minDiskGb >= 0, "minDiskGb needs to be >= 0");
+        checkArgument(maxDiskGb >= 0, "maxDiskGb needs to be >= 0");
     }
 
 }
