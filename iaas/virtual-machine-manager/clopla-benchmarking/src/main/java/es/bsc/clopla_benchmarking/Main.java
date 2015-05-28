2e78c86a0bbe02213df1cad14b9ce805729dc866
David Ortiz <david.ortiz@bsc.es>
2015-05-28 15:31:18 +0000

    Add simple test

--- iaas/virtual-machine-manager/clopla-benchmarking/src/main/java/es/bsc/clopla_benchmarking/Main.java
+++ iaas/virtual-machine-manager/clopla-benchmarking/src/main/java/es/bsc/clopla_benchmarking/Main.java
@@ -2,9 +2,7 @@ package es.bsc.clopla_benchmarking;
 
 import es.bsc.clopla.domain.Host;
 import es.bsc.clopla.domain.Vm;
-import es.bsc.clopla.placement.config.localsearch.HillClimbing;
-import es.bsc.clopla.placement.config.localsearch.LocalSearch;
-import es.bsc.clopla.placement.config.localsearch.StepCountingHC;
+import es.bsc.clopla.placement.config.localsearch.*;
 import es.bsc.clopla_benchmarking.cluster_generation.ClusterGenerator;
 import es.bsc.clopla_benchmarking.cluster_generation.HostDimensions;
 import es.bsc.clopla_benchmarking.experiments.Experiment;
@@ -21,23 +19,45 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Cluster cluster = ClusterGenerator.generateCluster(1000, 50, 1000, new HostDimensions(1, 16, 1, 16, 10, 100));
+        int[] times = {60, 180, 300};
 
-        // Assign the hosts randomly, because we are only interested in comparing
-        // the local search heuristics, not the construction heuristics (at least for now)
-        assignVmsToHostRandomly(cluster.getVms(), cluster.getHosts());
+        List<Cluster> clusters = new ArrayList<>();
+
+        clusters.add(ClusterGenerator.generateCluster(100, 25, 100, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(100, 50, 100, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(100, 75, 100, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(100, 100, 100, new HostDimensions(1, 16, 1, 16, 10, 100)));
+
+        clusters.add(ClusterGenerator.generateCluster(1000, 25, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(1000, 50, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(1000, 75, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(1000, 100, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
+
+        clusters.add(ClusterGenerator.generateCluster(1000, 25, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(1000, 50, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(1000, 75, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
+        clusters.add(ClusterGenerator.generateCluster(1000, 100, 1000, new HostDimensions(1, 16, 1, 16, 10, 100)));
 
         List<LocalSearch> localSearchAlgs = new ArrayList<>();
         localSearchAlgs.add(new HillClimbing());
-        //localSearchAlgs.add(new LateAcceptance(400));
-        //localSearchAlgs.add(new LateSimulatedAnnealing(100, 1000));
-        //localSearchAlgs.add(new SimulatedAnnealing(2, 100));
+        localSearchAlgs.add(new LateAcceptance(400));
+        localSearchAlgs.add(new LateSimulatedAnnealing(100, 1000));
+        localSearchAlgs.add(new SimulatedAnnealing(2, 100));
         localSearchAlgs.add(new StepCountingHC(400));
-        //localSearchAlgs.add(new TabuSearch(7, 1000));
+        localSearchAlgs.add(new TabuSearch(7, 1000));
 
-        Experiment experiment = ExperimentGenerator.generateExperiment(cluster, 120, localSearchAlgs);
-        System.out.println(ResultsCsvConverter.experimentExecutionResultsToCsv(
-                ExperimentRunner.runExperiment(experiment)));
+
+        for (Cluster cluster : clusters) {
+            assignVmsToHostRandomly(cluster.getVms(), cluster.getHosts());
+            List<Vm> originalVmList = new ArrayList<>(cluster.getVms());
+
+            for (int time : times) {
+                Experiment experiment = ExperimentGenerator.generateExperiment(
+                        new Cluster(originalVmList, cluster.getHosts()), time, localSearchAlgs);
+                System.out.println(ResultsCsvConverter.experimentExecutionResultsToCsv(
+                        ExperimentRunner.runExperiment(experiment)));
+            }
+        }
     }
 
     private static void assignVmsToHostRandomly(List<Vm> vms, List<Host> hosts) {
