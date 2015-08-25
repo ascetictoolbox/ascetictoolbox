14a7dd91fa72fcd6f3287fd33b84da7e9b51dd50
David Ortiz <david.ortiz@bsc.es>
2015-08-25 14:54:33 +0000

    Comment function call that crashes in the energy modeller

--- iaas/virtual-machine-manager/vmmanagercore/src/main/java/es/bsc/vmmanagercore/manager/components/VmsManager.java
+++ iaas/virtual-machine-manager/vmmanagercore/src/main/java/es/bsc/vmmanagercore/manager/components/VmsManager.java
@@ -220,9 +220,10 @@ public class VmsManager {
                 initializeVmBilling(vmId, hostForDeployment.getHostname());
             }
 
-            if (energyModeller instanceof AsceticEnergyModellerAdapter) {
+            // Commented code because the Energy Modeller crashes.
+            /*if (energyModeller instanceof AsceticEnergyModellerAdapter) {
                 ((AsceticEnergyModellerAdapter) energyModeller).initializeVmInEnergyModellerSystem(vmId);
-            }
+            }*/
 
             if (vmToDeploy.needsFloatingIp()) {
                 cloudMiddleware.assignFloatingIp(vmId);
