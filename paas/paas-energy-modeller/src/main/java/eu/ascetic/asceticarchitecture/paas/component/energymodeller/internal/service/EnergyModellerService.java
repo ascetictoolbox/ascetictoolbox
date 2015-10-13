1cf1d4acfeceb1c13078538d9a7f02a9d70769f1
Davide Sommacampagna <davide.sommacampagna@hpe.com>
2015-10-13 14:25:29 +0000

    Summary: license and freeze release Comments: license and freeze release

--- paas/paas-energy-modeller/src/main/java/eu/ascetic/asceticarchitecture/paas/component/energymodeller/internal/service/EnergyModellerService.java
+++ paas/paas-energy-modeller/src/main/java/eu/ascetic/asceticarchitecture/paas/component/energymodeller/internal/service/EnergyModellerService.java
@@ -1,5 +1,17 @@
 /**
-* Copyright 2014 Hewlett-Packard Development Company, L.P.                                         
+   Copyright 2014-2015 Hewlett-Packard Development Company, L.P.  
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
  */
 package eu.ascetic.asceticarchitecture.paas.component.energymodeller.internal.service;
 
@@ -752,19 +764,6 @@ public class EnergyModellerService implements PaaSEnergyModeller {
 		LOGGER.debug("Configured");
 	}
 	
-// 
-//	@Override
-//	public double measure(String providerid, String applicationid,
-//			List<String> vmids, String eventid, Unit unit, Timestamp start,
-//			Timestamp end) {
-//		
-//		LOGGER.info("Requested Provider"+providerid+ " now application ");
-//		LOGGER.info("Requested Application "+applicationid+" now deployment");
-//		LOGGER.info("This is just a workadount of a deprecated IF");
-//		return this.measure(null, providerid, applicationid,vmids, eventid, unit, start,  end);
-//			
-//	}
-
 
 
 		
