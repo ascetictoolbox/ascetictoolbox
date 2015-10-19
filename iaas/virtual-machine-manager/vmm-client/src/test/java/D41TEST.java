15b6ce8b94ba993515a2d5b356c96b6468af588b
Mario Macias <mario.macias@bsc.es>
2015-10-19 14:30:58 +0000

    more on tests

--- iaas/virtual-machine-manager/vmm-client/src/test/java/D41TEST.java
+++ iaas/virtual-machine-manager/vmm-client/src/test/java/D41TEST.java
@@ -1,7 +1,4 @@
-import es.bsc.vmmclient.models.ImageUploaded;
-import es.bsc.vmmclient.models.Node;
-import es.bsc.vmmclient.models.VmCost;
-import es.bsc.vmmclient.models.VmDeployed;
+import es.bsc.vmmclient.models.*;
 import es.bsc.vmmclient.vmm.VmManagerClient;
 import junit.framework.TestCase;
 import org.junit.Ignore;
@@ -20,20 +17,40 @@ public class D41TEST extends TestCase {
 	public void testGetVMMInfo() {
 		VmManagerClient vmm = new VmManagerClient(URL_D41);
 
-		System.out.println("* Uploaded images");
-		for(ImageUploaded img : vmm.getImages()) {
-			System.out.println("\t"+img.toString());
-		}
-
-		System.out.println("* Nodes");
-		for(Node node : vmm.getNodes()) {
-			System.out.println("\t"+node.toString());
-		}
+//		System.out.println("* Uploaded images");
+//		for(ImageUploaded img : vmm.getImages()) {
+//			System.out.println("\t"+img.toString());
+//		}
+//
+//		System.out.println("* Nodes");
+//		for(Node node : vmm.getNodes()) {
+//			System.out.println("\t"+node.toString());
+//		}
 
 		System.out.println("* Deployed vms");
 		for(VmDeployed vm : vmm.getVms()) {
 			System.out.println("\t"+vm.toString());
 		}
+
+		System.out.println("* deleting all vms");
+		for(VmDeployed vm : vmm.getVms()) {
+			System.out.println("deleting " + vm.getName() + "... ");
+			vmm.destroyVm(vm.getId());
+		}
+
+
+			// deploy vms
+//		for(int i = 1 ; i <= 4 ; i++) {
+//			System.out.println("deploying vm " + i);
+//			Vm vm = new Vm("d41vm"+i,
+//					"db040522-e744-40e2-af4d-e9ee6c21f18b",
+//					2,1024,512,512,null,null,null,null,false);
+//
+//			List<String> vmId = vmm.deployVms(Arrays.asList(vm));
+//
+//			VmDeployed d = vmm.getVm(vmId.get(0));
+//			System.out.println("\tdeployed: " + d.toString());
+//		}
 
 	}
 
