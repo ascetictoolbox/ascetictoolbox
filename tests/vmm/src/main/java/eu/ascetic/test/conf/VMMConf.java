8602df1bf2be0a4295277e10b86927f60ebd676a
Raimon Bosch <raimon.bosch@gmail.com>
2016-03-09 11:14:19 +0000

    configuration files + deploy and migrate test

---
+++ tests/vmm/src/main/java/eu/ascetic/test/conf/VMMConf.java
@@ -0,0 +1,50 @@
+package eu.ascetic.test.conf;
+
+import java.io.File;
+
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.log4j.Logger;
+
+
+/**
+ * 
+ * Copyright (C) 2013-2014  Barcelona Supercomputing Center 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @author Raimon Bosch (raimon.bosch@bsc.es)
+ */
+public class VMMConf {
+	private static Logger logger = Logger.getLogger(VMMConf.class);
+	public static String vmManagerURL = "";
+    public static String imageId = "";
+	
+	static {
+        try {
+        	String propertiesFile = "config.properties";
+        	
+        	File f = new File(propertiesFile);
+        	if(f.exists()) {
+        		logger.info("ASCETiC tests configuration file: " + f.getAbsolutePath());
+        	}
+        	
+        	org.apache.commons.configuration.Configuration config = new PropertiesConfiguration(propertiesFile);
+        	vmManagerURL = config.getString("vm.manager.url");
+            imageId = config.getString("image.id");
+        }
+        catch (Exception e) {
+            logger.info("Error loading ASCETiC Tests configuration file");
+            logger.info("Exception " + e);
+        }
+	}
+}
---
+++ tests/vmm/src/test/java/eu/ascetic/test/iaas/vmm/DeployAndMigrateTest.java
@@ -0,0 +1,92 @@
+package eu.ascetic.test.iaas.vmm;
+
+import es.bsc.vmmclient.models.Node;
+import es.bsc.vmmclient.models.Vm;
+import es.bsc.vmmclient.models.VmDeployed;
+import es.bsc.vmmclient.vmm.VmManagerClient;
+import eu.ascetic.test.conf.VMMConf;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Logger;
+import junit.framework.TestCase;
+
+/**
+ * @author Raimon Bosch (http://github.com/raimonbosch), 
+ *         Mario Macías (http://github.com/mariomac)
+ */
+public class DeployAndMigrateTest extends TestCase{
+    private static final Logger logger = Logger.getLogger("DeployAndMigrateTest");
+    
+    VmManagerClient vmm;
+    
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        vmm = new VmManagerClient(VMMConf.vmManagerURL);
+    }
+
+    public void testDeployMigrateAndDestroy() throws Exception {
+        int cpus = 1;
+        int ramMb = 256;
+        int swapMb = 16;
+        int diskGb = 1;
+        
+        List<Node> nodes = new ArrayList<Node>();
+        for (Node node : vmm.getNodes()) {
+			if( (node.getTotalCpus() - node.getAssignedCpus()) >= cpus && 
+                (node.getTotalMemoryMb() - node.getAssignedMemoryMb()) >= ramMb && 
+                (node.getTotalDiskGb() - node.getAssignedDiskGb()) >= diskGb){
+                nodes.add(node);
+            }
+		}
+        
+        assertTrue("Can't run test with less than 2 compute nodes with enough resources!", 
+            nodes.size() >= 2);
+        
+        String vmName = "deployAndMigrateTest01";
+        String computeNode01 = nodes.get(0).getHostname();
+        String computeNode02 = nodes.get(1).getHostname();
+        
+        //Deploy
+        logger.info("Deploying '" + vmName + "' at " + computeNode01 + "...");
+        Vm vm = new Vm("deployAndMigrateTest01", VMMConf.imageId, cpus, ramMb, diskGb, swapMb, 
+                null, "dmt01", "", "sla", computeNode01);
+		List<String> deployedVms = vmm.deployVms(Arrays.asList(vm));
+		VmDeployed vmd = vmm.getVm(deployedVms.get(0));
+        String vmId = vmd.getId();
+        
+        assertEquals("ACTIVE", vmd.getState());
+        assertEquals(computeNode01, vmd.getHostName());
+        assertEquals(cpus, vmd.getCpus());
+        assertEquals(diskGb, vmd.getDiskGb());
+        assertEquals(ramMb, vmd.getRamMb());
+        assertEquals(swapMb, vmd.getSwapMb());
+        
+        //Migrate
+        logger.info("Deployed " + vmName + " with id:" + vmId + 
+            ". Migrating VM to " + computeNode02 + "...");
+        vmm.migrate(deployedVms.get(0), computeNode02);
+        
+        VmDeployed vmMigrated = null;
+        while(vmm.getVm(vmId).getState().equals("MIGRATING")) {
+            logger.info("Waiting migration to finish...");
+            Thread.sleep(2500);
+        }
+        
+        vmMigrated = vmm.getVm(vmId);
+        assertEquals("ACTIVE", vmMigrated.getState());
+        assertEquals(computeNode02, vmMigrated.getHostName());
+        assertEquals(cpus, vmMigrated.getCpus());
+        assertEquals(diskGb, vmMigrated.getDiskGb());
+        assertEquals(ramMb, vmMigrated.getRamMb());
+        assertEquals(swapMb, vmMigrated.getSwapMb());
+        
+        //Destroy
+        logger.info("Destroying VM " + vmId);
+        vmm.destroyVm(vmId);
+        
+        VmDeployed vmDestroyed = vmm.getVm(vmId);
+        assertNull(vmDestroyed);
+    }
+}
