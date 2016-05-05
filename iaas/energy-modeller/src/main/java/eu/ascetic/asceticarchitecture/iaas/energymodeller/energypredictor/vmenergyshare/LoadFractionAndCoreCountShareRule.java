643d73bdf2ab3b68f519b253ac0f20e232d5c390
Richard Kavanagh <scsrek@leeds.ac.uk>
2016-05-05 08:57:33 +0000

    Summary: Adding a share rule that considers both load and cpu core count.

---
+++ iaas/energy-modeller/src/main/java/eu/ascetic/asceticarchitecture/iaas/energymodeller/energypredictor/vmenergyshare/LoadFractionAndCoreCountShareRule.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright 2016 University of Leeds
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package eu.ascetic.asceticarchitecture.iaas.energymodeller.energypredictor.vmenergyshare;
+
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.queryinterface.datasourceclient.VmMeasurement;
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.Host;
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.VM;
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.VmDeployed;
+import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.usage.HostVmLoadFraction;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This looks at the fraction of load placed on each VM and determines the 
+ * share of energy that is should have based upon this. It also accounts for 
+ * the CPU core count so that a VM with more cores takes a greater fraction of
+ * the overall power consumption.
+ * @author Richard Kavanagh
+ */
+public class LoadFractionAndCoreCountShareRule implements EnergyShareRule {
+
+    private HashMap<VmDeployed, Double> fractions = new HashMap<>();
+    
+    @Override
+    public EnergyDivision getEnergyUsage(Host host, Collection<VM> vms) {
+        EnergyDivision answer = new EnergyDivision(host);
+        for (VM vm : vms) {
+            VmDeployed deployed = (VmDeployed) vm;
+            answer.addVmWeight(vm, fractions.get(deployed));
+            Logger.getLogger(LoadFractionAndCoreCountShareRule.class.getName()).log(Level.FINE, "VM: {0} Ratio: {1}", new Object[]{deployed.getName(), fractions.get(deployed)});
+        }
+        return answer;
+    }
+
+    /**
+     * This sets the load fractions to use in this energy share rule.
+     * @param vmMeasurements The Vm measurements that are used to set this load
+     * fraction data.
+     */
+    public void setVmMeasurements(List<VmMeasurement> vmMeasurements) {
+        fractions = HostVmLoadFraction.getFraction(vmMeasurements, true);
+    }   
+
+    /**
+     * This returns the data that indicates which VMs should take which fraction
+     * of the overall energy.
+     * @return the fractioning of the host energy data.
+     */
+    public HashMap<VmDeployed, Double> getFractions() {
+        return fractions;
+    }
+
+    /**
+     * This allows the data that indicates which VMs should take which fraction
+     * of the overall energy to be directly set.
+     * @param fractions the fractioning of the host energy data to set.
+     */
+    public void setFractions(HashMap<VmDeployed, Double> fractions) {
+        this.fractions = fractions;
+    }
+
+}
