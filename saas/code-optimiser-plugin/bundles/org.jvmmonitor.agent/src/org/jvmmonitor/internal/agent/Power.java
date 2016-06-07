125e71bc56856ff955f02b585e3b3a1d04dcb82f
Richard Kavanagh <scsrek@leeds.ac.uk>
2016-06-07 14:27:41 +0000

    Summary: Updating the code optimiser plug-in to work with the energy modeller's predictor methods. Comments: The calibration data is currently a temporary fix and will need a mechanism to load in a set of calibration data.

--- saas/code-optimiser-plugin/bundles/org.jvmmonitor.agent/src/org/jvmmonitor/internal/agent/Power.java
+++ saas/code-optimiser-plugin/bundles/org.jvmmonitor.agent/src/org/jvmmonitor/internal/agent/Power.java
@@ -7,7 +7,7 @@ import eu.ascetic.asceticarchitecture.iaas.energymodeller.energypredictor.CpuOnl
 import eu.ascetic.asceticarchitecture.iaas.energymodeller.energypredictor.EnergyPredictorInterface;
 import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.Host;
 import eu.ascetic.asceticarchitecture.iaas.energymodeller.types.energyuser.usage.HostEnergyCalibrationData;
-
+import org.apache.commons.configuration.PropertiesConfiguration;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,13 +18,13 @@ public class Power implements PowerMXBean {
     public final static String POWER_MXBEAN_NAME = "org.jvmmonitor:type=Power";
     private OperatingSystemMXBean operatingSystemMXBean;
     //private String predictorName = "CpuOnlyBestFitEnergyPredictor";
-    private EnergyPredictorInterface predictor = null;  //getPredictor(predictorName);
-    private static final String DEFAULT_PREDICTOR_PACKAGE = "eu.ascetic.asceticarchitecture.iaas.energymodeller.energypredictor";    
+    private EnergyPredictorInterface predictor = null; // getPredictor(predictorName);
+    private static final String DEFAULT_PREDICTOR_PACKAGE = "eu.ascetic.asceticarchitecture.iaas.energymodeller.energypredictor";
     private ArrayList<HostEnergyCalibrationData> calibrationData = new ArrayList<>();
-    
+
     private long nanoBefore = 0;
     private long cpuBefore = 0;
-    Host host = new Host(0, "localhost"); 
+    Host host = new Host(0, "localhost");
 
     /**
      * The constructor.
@@ -42,8 +42,15 @@ public class Power implements PowerMXBean {
         host.setDiskGb(20);
         host.setRamMb(2048);
         host.setCalibrationData(calibrationData);
-        //TODO Ensure the predictor doesn't take settings from disk
-        //predictor = new CpuOnlyBestFitEnergyPredictor();
+        PropertiesConfiguration config = new PropertiesConfiguration();
+        config.setProperty("iaas.energy.modeller.cpu.energy.predictor.default_load", 0);
+        config.setProperty("iaas.energy.modeller.cpu.energy.predictor.vm_share_rule", "DefaultEnergyShareRule");
+        config.setProperty("iaas.energy.modeller.cpu.energy.predictor.consider_idle_energy", true);
+        config.setProperty("iaas.energy.modeller.energy.predictor.overheadPerHostInWatts", 0);
+        config.setProperty("iaas.energy.modeller.cpu.energy.predictor.datasource", "ZabbixDirectDbDataSourceAdaptor");
+        config.setProperty("iaas.energy.modeller.cpu.energy.predictor.utilisation.observe_time.min", 0);
+        config.setProperty("iaas.energy.modeller.cpu.energy.predictor.utilisation.observe_time.sec", 30);
+        predictor = new CpuOnlyBestFitEnergyPredictor(config);
     }
 
     /**
@@ -54,9 +61,9 @@ public class Power implements PowerMXBean {
     public boolean isSupported() {
         return true;
     }
-    
+
     public void setHostCalibrationData(ArrayList<HostEnergyCalibrationData> calibrationData) {
-    	this.calibrationData = calibrationData;
+        this.calibrationData = calibrationData;
     }
 
     /**
@@ -65,36 +72,40 @@ public class Power implements PowerMXBean {
      * @return the value for power
      */
     public double getPower() {
+        try {
+            if (predictor != null) {
+                long nanoAfter = System.nanoTime();
+                long cpuAfter = getProcessCpuTime();
 
-      if (predictor != null) {
-        long nanoAfter = System.nanoTime();
-        long cpuAfter = getProcessCpuTime();
+                double cpuPercentage = 0.0;
+                if (nanoAfter > nanoBefore) {
+                    cpuPercentage = ((cpuAfter - cpuBefore) * 100.0) / (double) (nanoAfter - nanoBefore);
+                }
 
-        double cpuPercentage = 0.0;
-        if (nanoAfter > nanoBefore) {
-            cpuPercentage = ((cpuAfter - cpuBefore) * 100.0)
-                    / (double) (nanoAfter - nanoBefore);
+                nanoBefore = nanoAfter;
+                cpuBefore = cpuAfter;
+
+                double power = 0.0;
+                if (!host.isCalibrated()) {
+                    host.setCalibrationData(calibrationData);
+                }
+                power = predictor.predictPowerUsed(host, cpuPercentage);
+                return power;
+            } else {
+                return 10;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return 5;
         }
-
-        nanoBefore = nanoAfter;
-        cpuBefore = cpuAfter;
-      
-        double power = 0.0;
-        if (!host.isCalibrated()) {
-        	host.setCalibrationData(calibrationData);  
-        }
-        power = predictor.predictPowerUsed(host, cpuPercentage);        
-    	return power;
-     } else {	 
-    		return 10;
-     }
 
     }
-    
+
     /**
      * This allows the power estimator to be set
      *
-     * @param powerUtilisationPredictor The name of the predictor to use
+     * @param powerUtilisationPredictor
+     *            The name of the predictor to use
      * @return The predictor to use.
      */
     public EnergyPredictorInterface getPredictor(String powerUtilisationPredictor) {
@@ -116,26 +127,22 @@ public class Power implements PowerMXBean {
             Logger.getLogger(Power.class.getName()).log(Level.WARNING, "The predictor specified did not work", ex);
         }
         return answer;
-    }       
+    }
 
     private long getProcessCpuTime() {
         try {
-            if (Class.forName("com.sun.management.OperatingSystemMXBean")
-                    .isInstance(operatingSystemMXBean)) {
-                Method processCpuTime = operatingSystemMXBean.getClass()
-                        .getDeclaredMethod("getProcessCpuTime");
+            if (Class.forName("com.sun.management.OperatingSystemMXBean").isInstance(operatingSystemMXBean)) {
+                Method processCpuTime = operatingSystemMXBean.getClass().getDeclaredMethod("getProcessCpuTime");
                 processCpuTime.setAccessible(true);
                 long time = (Long) processCpuTime.invoke(operatingSystemMXBean);
-                return time; 
+                return time;
             } else {
                 // FIXME Add alternative method if sun packages is not available
                 System.err.println("Reflection using com.sun.management.OperatingSystemMXBean failed");
                 return 0;
             }
         } catch (Exception e) {
-            System.err.println(
-                    "Error invoking getProcessCpuTime() by reflection: "
-                            + e.getMessage());
+            System.err.println("Error invoking getProcessCpuTime() by reflection: " + e.getMessage());
             return 0;
         }
     }
