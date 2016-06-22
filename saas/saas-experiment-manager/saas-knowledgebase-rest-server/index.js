5fd0b0818f4f81e2cc3834cb8a669eb8f5e73d82
Dimitri Durieux <dimitri.durieux@cetic.be>
2016-06-22 08:34:56 +0000

    add dockerfile for skb

--- saas/saas-experiment-manager/saas-knowledgebase-rest-server/index.js
+++ saas/saas-experiment-manager/saas-knowledgebase-rest-server/index.js
@@ -5,7 +5,7 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var serverPort = 8080;
+var serverPort = process.env.SERVER_PORT;
 
 // swaggerRouter configuration
 var options = {
