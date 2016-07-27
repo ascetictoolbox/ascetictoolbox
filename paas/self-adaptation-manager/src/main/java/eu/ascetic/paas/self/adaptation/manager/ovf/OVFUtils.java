148d7d9194d681a39e04ce989401bef0dede9a96
Richard Kavanagh <scsrek@leeds.ac.uk>
2016-07-27 15:52:39 +0000

    Summary: Adding the ability to discover the amount of hard disk space that is required.

--- paas/self-adaptation-manager/src/main/java/eu/ascetic/paas/self/adaptation/manager/ovf/OVFUtils.java
+++ paas/self-adaptation-manager/src/main/java/eu/ascetic/paas/self/adaptation/manager/ovf/OVFUtils.java
@@ -17,14 +17,24 @@
 package eu.ascetic.paas.self.adaptation.manager.ovf;
 
 import es.bsc.vmmclient.models.VmRequirements;
+import eu.ascetic.utils.ovf.api.Disk;
+import eu.ascetic.utils.ovf.api.DiskSection;
+import eu.ascetic.utils.ovf.api.Item;
 import eu.ascetic.utils.ovf.api.OvfDefinition;
 import eu.ascetic.utils.ovf.api.ProductSection;
 import eu.ascetic.utils.ovf.api.VirtualSystem;
 import eu.ascetic.utils.ovf.api.VirtualSystemCollection;
+import eu.ascetic.utils.ovf.api.enums.ResourceType;
 import eu.ascetic.utils.ovf.api.utils.OvfRuntimeException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.apache.log4j.Logger;
 
-
+/**
+ * This class provides services for the interpretation of OVF.
+ * @author Richard Kavanagh
+ */
 public class OVFUtils {
 
     /**
@@ -125,8 +135,8 @@ public class OVFUtils {
                 if (ovfId.equals(ovfVirtualSystemID)) {
                     int cpu = virtualSystem.getVirtualHardwareSection().getNumberOfVirtualCPUs();
                     int ram = virtualSystem.getVirtualHardwareSection().getMemorySize();
-                    int disk = 0; //TODO
-                    int swap = 0; //TODO
+                    int disk = (int) getVmDiskSize(virtualSystem, ovf.getDiskSection());
+                    int swap = 0; //TODO - Swap is not specified in the OVF
                     return new VmRequirements(cpu, ram, disk, swap);
                 }
             }
@@ -135,6 +145,78 @@ public class OVFUtils {
             ex.printStackTrace();
         }
         return null;
+    }
+    
+    /**
+     * This returns the size of the overall disk space available to the VM.
+     *
+     * @param diskSection The set of disks as defined in the OVF
+     * @param virtualMachine The virtual machine to find the disk size for
+     * @return The size of this disk in Gb
+     */
+    private static double getVmDiskSize(VirtualSystem virtualMachine, DiskSection diskSection) {
+        double answer = 0.0f;
+        List<Disk> disks = Arrays.asList(diskSection.getDiskArray());
+        ArrayList<String> diskIds = getVMsDiskIds(virtualMachine);
+        for (Disk disk : disks) {
+            if (diskIds.contains(disk.getDiskId())) {
+                answer = answer + getDiskCapacity(disk);
+            }
+        }
+        return answer / Math.pow(2, 30);
+    }
+    
+    /**
+     * This returns the disk capacity in bytes.
+     *
+     * @param disk The disk to obtain the size information for
+     * @return The disk's capacity in bytes.
+     */
+    private static double getDiskCapacity(Disk disk) {
+        double answer = Double.parseDouble(disk.getCapacity());
+        if (disk.getCapacityAllocationUnits() != null) {
+            switch (disk.getCapacityAllocationUnits()) {
+                case "byte * 2^30": //Gb
+                    return answer * Math.pow(2, 30);
+                case "byte * 2^20": //MByte
+                    return answer * Math.pow(2, 20);
+                case "byte * 2^10": //KByte
+                    return answer * Math.pow(2, 10);
+            }
+        }
+        return answer;
+    }    
+    
+    /**
+     * This identifies from a VirtualSystem the disk ids associated.
+     *
+     * @param virtualMachine The virtual system to find the disk ids for
+     * @return The list of ids for the virtual machines disk.
+     */
+    private static ArrayList<String> getVMsDiskIds(VirtualSystem virtualMachine) {
+        ArrayList<String> ids = new ArrayList<>();
+        for (Item item : virtualMachine.getVirtualHardwareSection().getItemArray()) {
+            if (item.getResourceType().equals(ResourceType.DISK_DRIVE)) {
+                for (String id : Arrays.asList(item.getHostResourceArray())) {
+                    ids.add(findHostRosourceId(id));
+                }
+            }
+        }
+        return ids;
+    }
+    
+    /**
+     * Returns the ID of the referenced file or disk from a host resource URI.
+     *
+     * @param hostResource The host resource URI to find an ID for
+     * @return The the ID of the references file or disk
+     */
+    public static String findHostRosourceId(String hostResource) {
+        if (hostResource.lastIndexOf('/') != -1) {
+            return hostResource.substring(hostResource.lastIndexOf('/') + 1).replace("\n", "").trim();
+        } else {
+            return null;
+        }
     }    
     
     /**
