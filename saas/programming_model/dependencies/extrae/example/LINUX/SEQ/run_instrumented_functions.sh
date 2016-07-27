bb2d9de6dba3b0ac1cb461339f9195d6b9902f4b
Francesc Lordan <francesc.lordan@bsc.es>
2016-07-27 14:48:39 +0000

    Francesc: Removing PM Y2

--- saas/programming_model/dependencies/extrae/example/LINUX/SEQ/run_instrumented_functions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export EXTRAE_CONFIG_FILE=extrae-instrument-functions.xml
-export EXTRAE_HOME=@sub_PREFIXDIR@
-export LD_PRELOAD=${EXTRAE_HOME}/lib/libseqtrace.so
-
-./pi_instrumented_functions
