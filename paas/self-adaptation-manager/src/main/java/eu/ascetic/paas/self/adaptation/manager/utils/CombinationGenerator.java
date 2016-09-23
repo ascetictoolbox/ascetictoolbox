2daa20d71378dccf368c25360b0b79fe4791232c
Richard Kavanagh <scsrek@leeds.ac.uk>
2016-09-23 09:49:46 +0000

    Summary: Formatting code, to meet same pattern as rest of codebase, no functional changes.

--- paas/self-adaptation-manager/src/main/java/eu/ascetic/paas/self/adaptation/manager/utils/CombinationGenerator.java
+++ paas/self-adaptation-manager/src/main/java/eu/ascetic/paas/self/adaptation/manager/utils/CombinationGenerator.java
@@ -1,162 +1,186 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright 2016 Barcelona Super Computing Center
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package eu.ascetic.paas.self.adaptation.manager.utils;
 
-
-//--------------------------------------
-// Systematically generate combinations.
-//--------------------------------------
-
+/**
+ * Systematically generate combinations.
+ */
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ *
+ * @author Raimon
+ */
 public class CombinationGenerator {
 
-  private int[] a;
-  private int n;
-  private int r;
-  private BigInteger numLeft;
-  private BigInteger total;
+    private int[] a;
+    private int n;
+    private int r;
+    private BigInteger numLeft;
+    private BigInteger total;
 
-  //------------
-  // Constructor
-  //------------
-
-  public CombinationGenerator (int n, int r) {
-    if (r > n) {
-      throw new IllegalArgumentException ();
-    }
-    if (n < 1) {
-      throw new IllegalArgumentException ();
-    }
-    this.n = n;
-    this.r = r;
-    a = new int[r];
-    BigInteger nFact = getFactorial (n);
-    BigInteger rFact = getFactorial (r);
-    BigInteger nminusrFact = getFactorial (n - r);
-    total = nFact.divide (rFact.multiply (nminusrFact));
-    reset ();
-  }
-
-  //------
-  // Reset
-  //------
-
-  public void reset () {
-    for (int i = 0; i < a.length; i++) {
-      a[i] = i;
-    }
-    numLeft = new BigInteger (total.toString ());
-  }
-
-  //------------------------------------------------
-  // Return number of combinations not yet generated
-  //------------------------------------------------
-
-  public BigInteger getNumLeft () {
-    return numLeft;
-  }
-
-  //-----------------------------
-  // Are there more combinations?
-  //-----------------------------
-
-  public boolean hasMore () {
-    return numLeft.compareTo (BigInteger.ZERO) == 1;
-  }
-
-  //------------------------------------
-  // Return total number of combinations
-  //------------------------------------
-
-  public BigInteger getTotal () {
-    return total;
-  }
-
-  //------------------
-  // Compute factorial
-  //------------------
-
-  private static BigInteger getFactorial (int n) {
-    BigInteger fact = BigInteger.ONE;
-    for (int i = n; i > 1; i--) {
-      fact = fact.multiply (new BigInteger (Integer.toString (i)));
-    }
-    return fact;
-  }
-
-  //--------------------------------------------------------
-  // Generate next combination (algorithm from Rosen p. 286)
-  //--------------------------------------------------------
-
-  public int[] getNext () {
-
-    if (numLeft.equals (total)) {
-      numLeft = numLeft.subtract (BigInteger.ONE);
-      return a;
-    }
-
-    int i = r - 1;
-    while (a[i] == n - r + i) {
-      i--;
-    }
-    a[i] = a[i] + 1;
-    for (int j = i + 1; j < r; j++) {
-      a[j] = a[i] + j - i;
-    }
-
-    numLeft = numLeft.subtract (BigInteger.ONE);
-    return a;
-
-  }
-
-  public static List<String> getCombinations(String input)
-  {
-    int k = 0;
-    String[] elements = input.split("\\,");
-    List<String> combinations = new ArrayList<String>();
-
-    StringBuilder combination = new StringBuilder ();
-    for(int j=1; j <= elements.length; j++){
-        int[] indices;
-        CombinationGenerator x = new CombinationGenerator (elements.length, j);
-        while (x.hasMore ()) {
-          combination.delete(0, combination.length());
-          indices = x.getNext ();
-          for (int i = 0; i < indices.length; i++) {
-            if(i > 0){
-                combination.append (",");
-            }
-            combination.append (elements[indices[i]]);
-          }
-          combinations.add(combination.toString());
-          //System.out.println ("(" + k + ") " + combination.toString ());
-          k++;
+    /**
+     *
+     * @param n
+     * @param r
+     */
+    public CombinationGenerator(int n, int r) {
+        if (r > n) {
+            throw new IllegalArgumentException();
         }
+        if (n < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.n = n;
+        this.r = r;
+        a = new int[r];
+        BigInteger nFact = getFactorial(n);
+        BigInteger rFact = getFactorial(r);
+        BigInteger nminusrFact = getFactorial(n - r);
+        total = nFact.divide(rFact.multiply(nminusrFact));
+        reset();
     }
 
-    return (combinations);
-  }
-
-  public static void main(String... args)
-  {
-    String all_filters = "type:Obres Teatre,moment:Aquest Divendres,moment:Aquesta Nit," + 
-      "moment:Aquest Dissabte,moment:Aquest Diumenge,moment:Aquest Dimarts,moment:Aquest Dimecres," + 
-      "moment:Aquest Dijous,location:Teatre Romea,city:Barcelona,district:Ciutat Vella," + 
-      "day:Diumenge 29 Abril 2012,day:Dimarts 1 Maig 2012,day:Dimecres 2 Maig 2012,ymonth:Abril 2012," + 
-      "ymonth:Maig 2012,ymonth:Juny 2012,lang:ca";
-    List<String> combinations = CombinationGenerator.getCombinations(all_filters);
-    System.out.println( "combinations:" );
-    int i = 0;
-    for(String comb : combinations){
-      System.out.println( comb.toString() );
-      i++;
+    /**
+     *
+     */
+    public void reset() {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = i;
+        }
+        numLeft = new BigInteger(total.toString());
     }
-    System.out.println( "Generated a total of " + i + " combinations." );
-  }
+
+    /**
+     * Return number of combinations not yet generated
+     *
+     * @return
+     */
+    public BigInteger getNumLeft() {
+        return numLeft;
+    }
+
+    /**
+     * Are there more combinations?
+     *
+     * @return
+     */
+    public boolean hasMore() {
+        return numLeft.compareTo(BigInteger.ZERO) == 1;
+    }
+
+    /**
+     * Return total number of combinations
+     *
+     * @return
+     */
+    public BigInteger getTotal() {
+        return total;
+    }
+
+    /**
+     *
+     * @param n
+     * @return
+     */
+    private static BigInteger getFactorial(int n) {
+        BigInteger fact = BigInteger.ONE;
+        for (int i = n; i > 1; i--) {
+            fact = fact.multiply(new BigInteger(Integer.toString(i)));
+        }
+        return fact;
+    }
+
+    /**
+     * Generate next combination (algorithm from Rosen p. 286)
+     *
+     * @return
+     */
+    public int[] getNext() {
+
+        if (numLeft.equals(total)) {
+            numLeft = numLeft.subtract(BigInteger.ONE);
+            return a;
+        }
+
+        int i = r - 1;
+        while (a[i] == n - r + i) {
+            i--;
+        }
+        a[i] = a[i] + 1;
+        for (int j = i + 1; j < r; j++) {
+            a[j] = a[i] + j - i;
+        }
+
+        numLeft = numLeft.subtract(BigInteger.ONE);
+        return a;
+
+    }
+
+    /**
+     *
+     * @param input
+     * @return
+     */
+    public static List<String> getCombinations(String input) {
+        int k = 0;
+        String[] elements = input.split("\\,");
+        List<String> combinations = new ArrayList<>();
+
+        StringBuilder combination = new StringBuilder();
+        for (int j = 1; j <= elements.length; j++) {
+            int[] indices;
+            CombinationGenerator x = new CombinationGenerator(elements.length, j);
+            while (x.hasMore()) {
+                combination.delete(0, combination.length());
+                indices = x.getNext();
+                for (int i = 0; i < indices.length; i++) {
+                    if (i > 0) {
+                        combination.append(",");
+                    }
+                    combination.append(elements[indices[i]]);
+                }
+                combinations.add(combination.toString());
+                //System.out.println ("(" + k + ") " + combination.toString ());
+                k++;
+            }
+        }
+
+        return (combinations);
+    }
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String... args) {
+        String all_filters = "type:Obres Teatre,moment:Aquest Divendres,moment:Aquesta Nit,"
+                + "moment:Aquest Dissabte,moment:Aquest Diumenge,moment:Aquest Dimarts,moment:Aquest Dimecres,"
+                + "moment:Aquest Dijous,location:Teatre Romea,city:Barcelona,district:Ciutat Vella,"
+                + "day:Diumenge 29 Abril 2012,day:Dimarts 1 Maig 2012,day:Dimecres 2 Maig 2012,ymonth:Abril 2012,"
+                + "ymonth:Maig 2012,ymonth:Juny 2012,lang:ca";
+        List<String> combinations = CombinationGenerator.getCombinations(all_filters);
+        System.out.println("combinations:");
+        int i = 0;
+        for (String comb : combinations) {
+            System.out.println(comb);
+            i++;
+        }
+        System.out.println("Generated a total of " + i + " combinations.");
+    }
 }
