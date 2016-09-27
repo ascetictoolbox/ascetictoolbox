e17379bf5e2d446630ae81120b66404fdbd7df74
Spyros Papafragkos <papafrag@aueb.gr>
2016-09-27 05:40:50 +0000

    Summary: PaaS Pricing Modeller error correction Comments: <OPTIONAL Add additional comments as needed>

--- paas/paas-pricing-modeller/src/main/java/eu/ascetic/asceticarchitecture/paas/paaspricingmodeller/pricingschemes/PaaSPricingModellerPricingScheme.java
+++ paas/paas-pricing-modeller/src/main/java/eu/ascetic/asceticarchitecture/paas/paaspricingmodeller/pricingschemes/PaaSPricingModellerPricingScheme.java
@@ -17,10 +17,6 @@
 
 package eu.ascetic.asceticarchitecture.paas.paaspricingmodeller.pricingschemes;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
 
 import eu.ascetic.asceticarchitecture.paas.paaspricingmodeller.billing.PaaSPricingModellerRegistration;
 import eu.ascetic.asceticarchitecture.paas.paaspricingmodeller.queue.client.GenericPricingMessage.Unit;
@@ -195,14 +191,15 @@ public abstract class PaaSPricingModellerPricingScheme {
 	 //TESTED
 	public double getEnergy(VMinfo VM){
 	        double difference=3000;
-	        ZonedDateTime zdt1 = ZonedDateTime.ofInstant(Instant.ofEpochMilli(VM.getEnergyChargesAll().getTime().getTimeInMillis()), ZoneId.systemDefault());
+	      //  ZonedDateTime zdt1 = ZonedDateTime.ofInstant(Instant.ofEpochMilli(VM.getEnergyChargesAll().getTime().getTimeInMillis()), ZoneId.systemDefault());
 	     //   System.out.println(zdt1.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
-	        ZonedDateTime zdt2 = ZonedDateTime.ofInstant(Instant.ofEpochMilli(VM.getChangeTime().getTimeInMillis()), ZoneId.systemDefault());
+	     //   ZonedDateTime zdt2 = ZonedDateTime.ofInstant(Instant.ofEpochMilli(VM.getChangeTime().getTimeInMillis()), ZoneId.systemDefault());
 	     //   System.out.println("Scheme getEnergy " +zdt2.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
 	       try{
 	    	    EMInteraction response = new EMInteraction();
-	        	double energy = response.getEnergyofVM(VM.getAppID(), Integer.toString(VM.getDepID()), Integer.toString(VM.getVMid()), zdt1.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME),zdt2.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
-	        	VM.updateEnergyConsumption(energy/1000);
+	    	    double energy =1000;
+	       // 	double energy = response.getEnergyofVM(VM.getAppID(), Integer.toString(VM.getDepID()), Integer.toString(VM.getVMid()), zdt1.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME),zdt2.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
+	        //	VM.updateEnergyConsumption(energy/1000);
 	        	return energy/1000;
 	        }
 	       catch (Exception ex){
